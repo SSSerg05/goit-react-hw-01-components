adb3da945f42608923bd9216d7b5d6255288787f
SSSerg05 <ssserg05@gmail.com>
2023-06-19 23:18:50 +0300

    add statistics
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import { Profile } from "./Profile";
 import User from '../data/user.json';
 
+import { Statistics } from "./Statistics";
+import data from '../data/data.json';
+
 export const App = () => {
   return (
     <div
@@ -21,6 +24,8 @@ export const App = () => {
         stats={User.stats}
       />
 
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
 
     </div>
   );
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,29 @@
+import "../css/statistics.css";
+
+export const Statistics = (props) => {
+  const {title, stats} = props
+  return (
+    <section class="statistics">
+     <h2 class="title">{title}</h2>
+
+     <ul class="stat-list">
+       <li class="item">
+         <span class="label">{stats.label}</span>
+         <span class="percentage">{stats.percentage}</span>
+       </li>
+       <li class="item">
+         <span class="label">.mp3</span>
+         <span class="percentage">14%</span>
+       </li>
+       <li class="item">
+         <span class="label">.pdf</span>
+         <span class="percentage">41%</span>
+       </li>
+       <li class="item">
+         <span class="label">.mp4</span>
+         <span class="percentage">12%</span>
+       </li>
+     </ul>
+    </section>
+  );
+}
