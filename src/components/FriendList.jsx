3fce1d942a16ae6c69d319452545969e2a255514
SSSerg05 <ssserg05@gmail.com>
2023-06-20 14:25:35 +0300

    fix show status in Friends

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,22 +1,13 @@
 import PropTypes from 'prop-types'; // ES6
 import "../css/friendList.css"
 
-const isOnlineShow = (isOnline) => {
-  if (!isOnline) {
-    return <span className="status" style={{backgroundColor: 'red'} }></span>  
-  }
-  
-  return <span className="status" style={{ backgroundColor: 'green' }}></span>  
-  
-}
-
 const friendsShow = (friends) =>
   friends.map(({ id, isOnline, name, avatar }) => 
-      <li className="item-friends" key={id}>
-          { isOnlineShow(isOnline) }
-          <img className="avatar" src={ avatar } alt="User avatar" width="48" />
-          <p className="name">{ name }</p>
-      </li>)
+    <li className="item-friends" key={id}>
+      <span className="status" style={ {backgroundColor: isOnline ? 'green':'red'} }></span>
+      <img className="avatar" src={ avatar } alt="User avatar" width="48" />
+      <p className="name">{ name }</p>
+    </li>)
 
 
 export const FriendList = ({ friends } ) => {
